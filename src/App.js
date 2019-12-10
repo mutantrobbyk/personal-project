339387dff6e1a5ba4724c1cd01fc52095cebd516
Robert Knowles <robertknowles@Roberts-MacBook-Pro.local>
2019-12-10 10:22:52 -0700

    beginning of redux persist

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import routes from "./routes";
 import { withRouter } from "react-router-dom";
-import axios from 'axios'
-import {setUser} from '../src/ducks/reducer'
-import {connect} from 'react-redux'
+import axios from "axios";
+import { setUser } from "../src/ducks/reducer";
+import { connect } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   componentDidMount() {
-    axios.get('/auth/currentuser').then(res => {
-      this.props.setUser(res.data)
-    })
+    axios.get("/auth/currentuser").then(res => {
+      this.props.setUser(res.data);
+    });
   }
   render() {
     return (
-      <div  className="App">
-        <Nav history={this.props.history} />
-        <div className='nav_offset'></div>
-        {routes}
-      </div>
+      <HashRouter>
+        <Provider store={store}>
+          <div className="App">
+            <Nav history={this.props.history} />
+            <div className="nav_offset"></div>
+            {routes}
+          </div>
+        </Provider>
+      </HashRouter>
     );
   }
 }
 
-export default connect(null, {setUser})(withRouter(App));
+export default connect(null, { setUser })(withRouter(App));
