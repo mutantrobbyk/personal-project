f2eac5f6259bbee7d67ad2a0605ffe53c9be9561
mutantrobbyk <robertknowles02@msn.com>
2021-01-20 18:19:40 -0700

    finally got button override styling to work with material ui.  still need to have final placement and color.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,44 +2,40 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import Button from "@material-ui/core/Button";
-import clsx from "clsx";
 import { withStyles } from "@material-ui/core/styles";
 import Spring from "../images/noun_suspension_378329.png";
 import Piston from "../images/noun_Piston_103816 (1).png";
 import Pic from "../images/noun_Photo_1827280 (1).png";
 import Wrench from "../images/noun_Wrench_2558227 (1).png";
 
-const styles = {
-  project_gallery: {
-    backgroundColor: "rgb(219, 5, 5)",
-    textAlign: "center",
-    position: "absolute",
-    bottom: "0",
-    right: "0",
-    marginRight: "8%",
-    marginBottom: "15%",
-    color: "white",
-    opacity: "80%",
-    cursor: "pointer",
-    padding: "10px",
-    fontFamily: "Alegreya Sans, sans-serif",
-    textPrimary: "white"
-  },
-  textPrimary: {
-    color: "white"
+const styles = theme => ({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    width: 200,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
   }
-};
- class Home extends Component {
+})
+class Home extends Component {
+  
   render() {
-    const {classes} = this.props
+   const {classes} = this.props
+   console.log(classes)
     return (
       <div className="Home">
         <div className="main_image">
           <div className="mainPageImages" />
           <h1 className="SD">SYNDICATE DEVELOPMENT</h1>
           <p className="sub_1">Performance, Passion, Precision.</p>
-          <Button color="primary" variant="text" className={clsx(classes.project_gallery, classes.textPrimary)}>
-            <Link to="/projects">PROJECT GALLERY</Link>
+          <Button
+            className={classes.root}
+            href='#/projects'
+          >
+            PROJECT GALLERY
           </Button>
         </div>
         <hr />
@@ -107,4 +103,4 @@ const styles = {
   }
 }
 
-export default withStyles(styles)(Home)
+export default withStyles(styles, {withTheme: true})(Home);
